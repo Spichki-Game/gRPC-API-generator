3662e230793f558efafdc069baa1cdae2d95119a
SciBourne <bourne-sci-hack@yandex.ru>
2023-04-18 21:13:33 +0500

    FEAT: poetry run generate-api $SERVICE_NAME

--- src/grpc_api_generator/main.py
+++ src/grpc_api_generator/main.py
@@ -1,102 +1,65 @@
-import os
 import sys
 import subprocess
 
-from pathlib import Path
 from grpc_tools import protoc
 
 
-class ConstantSet:
-    def __init__(self, project_name: str, api_name: str):
-        self.project_name = project_name
+class GeneratorAPI:
+    PROTO_PATH: str = "src/protobuf"
+    API_PATH: str = "src/grpc_api"
 
-        for path in sys.path:
-            if project_name + '/src' in path:
+    def __init__(self, service_name: str):
+        self.service_name = service_name
 
-                self.API_NAME: str = (
-                    # self.project_name.lower().replace('-', '_')
-                    api_name
-                )
+    def run(self, echo: bool = False) -> None:
+        self.generate()
+        self.move()
+        self.patch()
+        self.print_complete() if echo else None
 
-                self.API_NAME_ALIAS: str = (
-                    self.API_NAME.replace('_', '__')
-                )
-
-                self.ROOT_DIR_PATH: Path = Path(
-                    # os.path.expanduser(path)
-                    path
-                )
-
-                self.PROTO_DIR_PATH: Path = Path.joinpath(
-                    self.ROOT_DIR_PATH,
-                    "proto_schemas"
-                )
-
-                self.PROTO_FILE_PATH: Path = Path.joinpath(
-                    self.PROTO_DIR_PATH,
-                    self.API_NAME + ".proto"
-                )
-
-                self.OUT_DIR_PATH: Path = Path.joinpath(
-                    self.ROOT_DIR_PATH,
-                    "grpc_api"
-                )
-
-                self.TARGET_FILE_PATH: Path = Path.joinpath(
-                    self.OUT_DIR_PATH,
-                    f"{self.API_NAME}_pb2_grpc.py"
-                )
-
-                self.INVALID_LINE: str = (
-                    f"import {self.API_NAME}_pb2 "
-                    f"as {self.API_NAME_ALIAS}__pb2"
-                )
-
-                self.FIX_LINE: str = (
-                    "from grpc_api "
-                    f"import {self.API_NAME}_pb2 "
-                    f"as {self.API_NAME_ALIAS}__pb2"
-                )
-
-                self.PATCH: list[str] = [
-                    "sed",
-                    "-i",
-                    f"s/{self.INVALID_LINE}/{self.FIX_LINE}/",
-                    f"{self.TARGET_FILE_PATH}"
-                ]
-
-                break
-
-
-def run(project_name: str, api_name: str, echo: bool = False) -> bool:
-    const = ConstantSet(project_name, api_name)
-
-    check = (
-        const.PROTO_FILE_PATH.exists(),
-        const.PROTO_FILE_PATH.is_file(),
-        const.PROTO_FILE_PATH.stat().st_size != 0
-    )
-
-    if not all(check):
-        raise ValueError("Proto file does not exists or empty")
-    else:
+    def generate(self) -> None:
         protoc.main(
-            [f"-I{os.getcwd()}",
-             f"--proto_path={const.PROTO_DIR_PATH}",
-             f"{const.PROTO_FILE_PATH}",
-             f"--python_out={const.OUT_DIR_PATH}",
-             f"--grpc_python_out={const.OUT_DIR_PATH}",
-             f"--pyi_out={const.OUT_DIR_PATH}"]
+            [f"--proto-path={self.PROTO_PATH}",
+             "--python_out=.",
+             "--grpc_python_out=.",
+             f"{self.PROTO_PATH}/{self.service_name}.proto"]
         )
 
-        subprocess.call(const.PATCH, shell=False)
+    def move(self) -> None:
+        file_names = [
+            f"{self.service_name}_pb2.py",
+            f"{self.service_name}_pb2_grpc.py"
+        ]
 
-        if echo:
-            print("[ OK ]: Complete")
-            print("[ Generated modules ]:",
-                  f"\t- {const.API_NAME}_pb2.py",
-                  f"\t- {const.API_NAME}_pb2_grpc.py",
-                  sep='\n',
-                  end='\n'*2)
+        for name in file_names:
+            subprocess.call(
+                ["mv",
+                 f"{self.PROTO_PATH}/{name}",
+                 f"{self.API_PATH}/{name}"]
+            )
 
-        return True
+    def patch(self) -> None:
+        target_file = f"{self.API_PATH}/{self.service_name}_pb2_grpc.py"
+        patch = "s/from src.protobuf/from grpc_api/"
+
+        subprocess.call(
+            ["sed", "-i", patch, target_file]
+        )
+
+    def print_complete(self) -> None:
+        api_files: str = subprocess.getoutput(
+            f"ls {self.API_PATH}"
+        )
+
+        print("[ Info ]: Generated api files:")
+
+        for name in api_files.split('\n'):
+            print(f" - {self.API_PATH}/{name}")
+
+
+def run(command_line_args: list = sys.argv) -> None:
+    try:
+        generator = GeneratorAPI(service_name=command_line_args[1])
+        generator.run(echo=True)
+    except IndexError:
+        print("[ Error ]: Missing service name parameter")
